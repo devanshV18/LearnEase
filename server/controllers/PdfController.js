c5de6ff62f634f73e9fd4b49ed5019fbf20eed8c
devanshV18 <devanshverma024@gmail.com>
2024-11-17 22:09:45 +0530

    alot of work to do guis

---
+++ server/controllers/PdfController.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+import {User} from "../models/userSchema.js"
+import {Pdf} from "../models/userSchema.js"
+import { v2 as cloudinary } from "cloudinary"
+
+export const uploadPdf = catchAsyncHnadler( async(req,res,next) => {
+
+})
+
+export const deletePdf = catchAsyncHnadler( async(req,res,next) => {
+    
+})
