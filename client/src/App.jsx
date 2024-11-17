87be856089e6e3edb2f6f5e526537d0cc4d61020
devanshV18 <devanshverma024@gmail.com>
2024-11-17 18:14:25 +0530

    need to settel up frontend better

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import RegistrationPage from './pages/RegistrationPage.jsx'
 import Navbar from './components/Navbar.jsx'
+import Spinner from './components/Spinner.jsx'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      {/* <RegistrationPage/> */}
+      {/* <Navbar/> */}
+      <RegistrationPage/>
+    
     </div>
   )
 }
