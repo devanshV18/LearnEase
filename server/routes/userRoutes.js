e37d2fe3ff25571c0921ea1158ef9f102135e38d
devanshV18 <devanshverma024@gmail.com>
2024-11-20 00:07:28 +0530

    modified user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,4 @@
-import { getProfile, register } from "../controllers/userController.js"
+import { getProfile, logout, register } from "../controllers/userController.js"
 import express from express
 import { isAuthenticated } from "../middlewares/auth.js"
 
@@ -9,5 +9,6 @@ const router = express.router()
 router.post("/register", register)
 router.post("/login", login)
 router.get("/getprofile", isAuthenticated, getProfile)
+router.get("/logout", isAuthenticated ,logout)
 
 export default router
